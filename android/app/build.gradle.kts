9f4b71535f7403372da138a13a10bb62608243c0
richardparulian <chardparulian@gmail.com>
2026-02-05 09:06:17 +0700

    change package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.cloudra.pos"
+    namespace = "com.pgi.patroli"
     compileSdk = 36
     ndkVersion = "27.0.12077973"
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.cloudra.pos"
+        applicationId = "com.pgi.patroli"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/pgi/patroli/MainActivity.kt
+++ android/app/src/main/kotlin/com/pgi/patroli/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.cloudra.pos
+package com.pgi.patroli
 
 import io.flutter.embedding.android.FlutterActivity
 
