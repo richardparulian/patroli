aa3b4f406db9c98fa85458ed04aa37e78e875a1a
richardparulian <chardparulian@gmail.com>
2026-03-08 01:24:01 +0700

    remove unused code

---
+++ .history/lib/features/scan_qr/data/dtos/response/scan_qr_response_20260308012325.dart
@@ -0,0 +1,17 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:pos/features/scan_qr/data/models/scan_qr_model.dart';
+
+part 'scan_qr_response.g.dart';
+
+@JsonSerializable(fieldRename: FieldRename.snake)
+class ScanQrResponse {
+  final ScanQrModel data;
+
+  const ScanQrResponse({
+    required this.data,
+  });
+
+  factory ScanQrResponse.fromJson(Map<String, dynamic> json) => _$ScanQrResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ScanQrResponseToJson(this);
+}
